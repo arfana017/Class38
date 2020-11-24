7b28a9da7db1ecd5c3a8de6760ebce9438329608
Arfana <arfanasham017@gmail.com>
2020-11-24 13:06:21 -0800

    form

--- sketch.js
+++ sketch.js
@@ -1,50 +1,24 @@
-var ball;
 
-var database, ballpos,position
+
+var database ;
+
+var gameState = 0;
+var playerCount;
+
+var form, player,game
+
 function setup(){
-    createCanvas(500,500);
-
+    createCanvas(400,400);
     database = firebase.database();
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    ballpos = database.ref('ball/position')
-    ballpos.on("value", readPos, showErr);
+    game = new Game();
+    game.getState();
+    game.start();
+    
 
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
+    
 }
 
-function writePosition(x,y){
-    database.ref('ball/position').set({
-        x: position.x+x,
-        y: position.y+y
-    })
-}
-
-function readPos(data){
-position = data.val();
-console.log(position)
-ball.x = position.x
-ball.y = position.y
-}
-
-function showErr(){
-
-    console.log("Error in reading the data ")
-}
 
