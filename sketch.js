e56bd26c1fdcd691a935fb68be118033226610c9
Arfana <arfanasham017@gmail.com>
2020-12-01 13:53:06 -0800

    real game display

--- sketch.js
+++ sketch.js
@@ -1,23 +1,36 @@
 
-
 var database ;
 
 var gameState = 0;
 var playerCount;
 
-var form, player,game
+var form, player,game,allPlayers;
+
+
+var car1,car2,car3,car4;
+
+var cars
 
 function setup(){
-    createCanvas(400,400);
+    
+    createCanvas(displayWidth-20,displayHeight- 30);
     database = firebase.database();
     game = new Game();
     game.getState();
     game.start();
     
-
 }
 
 function draw(){
+if(playerCount === 4){
+
+    game.update(1)
+}
+if(gameState === 1){
+    clear();
+    game.play();
+}
+
     
 }
 
